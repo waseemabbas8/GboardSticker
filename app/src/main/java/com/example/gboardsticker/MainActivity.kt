2eb6609f4b4f5729b272979dc0031541e6fc4086
WASEEM ABBAS <wajutt8@gmail.com>
2021-04-21 15:38:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/gboardsticker/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.gboardsticker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnAddStickers)
+                .setOnClickListener {
+                    AppIndexingUpdateService.enqueueWork(this);
+                }
+
+    }
+}
